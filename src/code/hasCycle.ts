0e16954a7d130947e7918e07171a0849090382e9
hanwenma <1768263460@qq.com>
2022-11-22 22:13:11 +0800

    add: add different hasCycle

--- src/code/hasCycle.ts
+++ src/code/hasCycle.ts
@@ -1,13 +1,42 @@
 import { ListNode } from "./mergeTwoLists";
 
 // 判断环形链表
-export function hasCycle(head) {
 
+// 立 flag 法
+export function hasCycle1(head) {
   while (head) {
     if (head.flag) return true;
-    
+
     head.flag = true;
     head = head.next;
+  }
+
+  return false;
+}
+
+// hash 表
+export function hasCycle2(head) {
+  const set = new Set();
+  while (head) {
+    if (set.has(head)) return true;
+
+    set.add(head);
+    head = head.next;
+  }
+
+  return false;
+}
+
+// 双指针
+export function hasCycle3(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+
+    if (slow === fast) return true;
+    slow = slow.next;
   }
 
   return false;
@@ -18,4 +47,6 @@ const head = new ListNode([1, 2, 3, 4, 5], true);
 // 形成环
 ListNode.last.next = head;
 
-console.log(hasCycle(head));
+console.log(hasCycle1(head));
+console.log(hasCycle2(head));
+console.log(hasCycle3(head));
