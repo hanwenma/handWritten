cc87618f02fbab25d443c430bbc0c609eb477ae9
hanwenma <1768263460@qq.com>
2022-12-07 23:34:15 +0800

    feat: finish quickSort

--- src/code/sortArray.ts
+++ src/code/sortArray.ts
@@ -224,6 +224,76 @@ function mergeArr(arr1, arr2) {
     return res.concat(arr2.slice(j));
   }
 }
-console.time();
-console.log("mergeSort = ", mergeSort(arr.slice()));
-console.timeEnd();
+// console.time();
+// console.log("mergeSort = ", mergeSort(arr.slice()));
+// console.timeEnd();
+
+/**
+ *
+ * 【快速排序】
+ * 时间复杂度：O(nlog(n)) —— O(n^2)
+ * 核心：
+ *      - 找基准值
+ *      - 比基准值小的放 左边
+ *      - 比基准值大的放 右边
+ */
+export function quickSort(arr, left = 0, right = arr.length - 1) {
+  // 递归边界
+  if (arr.length > 1) {
+    // lineIndex 表示下一次划分左右子数组的索引位
+    const lineIndex = partition(arr, left, right);
+
+    // 如果左边子数组的长度不小于 1，则递归快排这个子数组
+    if (left < lineIndex - 1) {
+      // 左子数组以 lineIndex-1 为右边界
+      quickSort(arr, left, lineIndex - 1);
+    }
+
+    // 如果右边子数组的长度不小于1，则递归快排这个子数组
+    if (right > lineIndex) {
+      // 右子数组以 lineIndex 为左边界
+      quickSort(arr, lineIndex, right);
+    }
+  }
+
+  return arr;
+}
+
+// 以基准值为轴心，划分左右子数组的过程
+function partition(arr, left, right) {
+  // 基准值默认取中间位置的元素
+  let pivotValue = arr[Math.floor(left + (right - left) / 2)];
+
+  // 初始化左右指针
+  let i = left;
+  let j = right;
+
+  // 当左右指针不越界时，循环执行以下逻辑
+  while (i <= j) {
+    // 左指针所指元素若小于基准值，则右移左指针
+    while (arr[i] < pivotValue) {
+      i++;
+    }
+
+    // 右指针所指元素大于基准值，则左移右指针
+    while (arr[j] > pivotValue) {
+      j--;
+    }
+
+    // 若 i<=j，则意味着基准值【左边】存在较大元素 或【右边】存在较小元素
+    // 交换两个元素确保左右两侧有序
+    if (i <= j) {
+      swap(arr, i, j);
+      i++;
+      j--;
+    }
+  }
+
+  // 返回左指针索引作为下一次划分左右子数组的依据
+  return i;
+}
+
+// 快速排序中使用 swap 的地方比较多，我们提取成一个独立的函数
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
