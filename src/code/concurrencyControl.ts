b202a31d18b1ccd1fcdac4773be821340ce8af8d
v_hanwma <v_hanwma@tencent.com>
2023-05-19 15:57:47 +0800

    fix: optimization concurrencyControl

--- src/code/concurrencyControl.ts
+++ src/code/concurrencyControl.ts
@@ -10,10 +10,12 @@
 // 生成用于测试的任务集合
 const tasks = new Array(10).fill(0).map((v, i) => {
     return function task() {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
                 console.log(`task ${i + 1} finish！`)
-                resolve(i + 1)
+                // resolve(i + 1)
+                if (i % 2 == 0) resolve(i + 1)
+                else reject(i + 1)
             }, i * 200);
         })
     }
@@ -24,7 +26,7 @@ const sendRequest = concurrencyControl(tasks, 3, () => {
     console.log(`task all finish！`)
 })
 
-sendRequest()
+//sendRequest()
 
 
 // 方式一
@@ -45,7 +47,7 @@ function concurrencyControl(tasks, limit, callback) {
 
                     if (count === tasks.length) {
                         callback()
-                    }else{
+                    } else {
                         runTask() // 继续执行下一个任务
                     }
                 })
@@ -57,6 +59,114 @@ function concurrencyControl(tasks, limit, callback) {
 }
 
 // 方式二
+class ConcurrencyControl1 {
+    queue: any[];
+    tasks: any[];
+    count: number;
+    limit: number;
+    callback: () => unknown;
+
+    constructor(tasks: any[], limit: number, callback: () => unknown) {
+        this.queue = tasks.slice()
+        this.tasks = tasks
+        this.count = 0
+        this.limit = limit
+        this.callback = callback
+    }
+
+    runTask() {
+        while (this.limit) {
+            this.limit--
+
+            if (this.queue.length) {
+                const task = this.queue.shift() // 取出队头任务
+
+                task()
+                    .finally(() => {
+                        this.limit++
+                        this.count++
+
+                        if (this.count === this.tasks.length) {
+                            this.callback() // 执行回调函数
+                        } else {
+                            this.runTask() // 继续执行下一个任务
+                        }
+                    })
+            }
+        }
+    }
+
+    addTask(task) {
+        // 同步添加任务
+        this.queue.push(task)
+        this.tasks.push(task)
+        // 当直接调用 addTask 也可直接执行
+        this.runTask()
+    }
+}
+
+// 实现方式三
+class ConcurrencyControl {
+    queue: Set<any>;
+    tasks: any[];
+    limit: number;
+    callback: () => unknown;
+
+    constructor(tasks: any[], limit: number, callback: () => unknown) {
+        this.tasks = tasks.slice()
+        this.queue = new Set()
+        this.limit = limit
+        this.callback = callback
+    }
+
+    runTask() {
+        if(this.tasks.length == 0) return
+
+        while (this.queue.size < this.limit) {
+
+            const task = this.tasks.shift() // 取出队头任务
+            this.queue.add(task) // 往队列中添加当前执行的任务
+
+            task()
+                .finally(() => {
+                    this.queue.delete(task) // 当前任务执行完毕，从队列中删除改任务
+
+                    if (this.queue.size == 0) {
+                        this.callback() // 执行回调函数
+                    } else {
+                        this.runTask() // 继续执行下一个任务
+                    }
+                })
+        }
+
+    }
+
+    addTask(task) {
+        // 同步添加任务
+        this.tasks.push(task)
+        // 当直接调用 addTask 也可直接执行
+        this.runTask()
+    }
+}
+
+// 测试代码
+const Control = new ConcurrencyControl(tasks, 3, () => {
+    console.log(`task all finish！`)
+})
+
+// 执行队列任务
+Control.runTask()
+
+// 添加新任务
+Control.addTask(function task() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`task 9999 finish！`)
+            resolve(999)
+        }, 100);
+    })
+})
+
 
 export default concurrencyControl
 
